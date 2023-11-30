10d0668c29b85d98830ebf1082a4dba3177c4abf
Dakshesh.Kololgi <dakshesh.kololgi.23@ucl.ac.uk>
2023-11-30 16:06:35 +0000

    Created 3D plot

--- Utilities.py
+++ Utilities.py
@@ -1,28 +1,41 @@
 import illustris_python as il
 import numpy as np
 import matplotlib.pyplot as plt
+import scienceplots
+plt.style.use('science')
 
-def readsnap(path, snapno, xyzplot=True, lim=500, colorbar=True):
+def readsnap(path, snapno, xyzplot=True, lim=5000):
     '''Reads the snapshot data from the given path and snapshot number.'''
     object = il.groupcat.load(path,snapno)
 
     if xyzplot:
-        X = object['halos']['GroupPos'][:,0][:lim] #spatial coordinates of halos
-        Y = object['halos']['GroupPos'][:,1][:lim]
-        Z = object['halos']['GroupPos'][:,2][:lim]
+        sf = object['header']['Time'] #time of snapshot
+        hub = object['header']['HubbleParam'] #hubble parameter of simulation
 
-        x = object['subhalos']['SubhaloPos'][:,0][:lim] #spatial coordinates of subhalos
-        y = object['subhalos']['SubhaloPos'][:,1][:lim]
-        z = object['subhalos']['SubhaloPos'][:,2][:lim]
+        X = object['halos']['GroupPos'][:,0][:lim]*sf/hub #spatial coordinates of halos
+        Y = object['halos']['GroupPos'][:,1][:lim]*sf/hub
+        Z = object['halos']['GroupPos'][:,2][:lim]*sf/hub
+
+        x = object['subhalos']['SubhaloPos'][:,0][:lim]*sf/hub #spatial coordinates of subhalos
+        y = object['subhalos']['SubhaloPos'][:,1][:lim]*sf/hub
+        z = object['subhalos']['SubhaloPos'][:,2][:lim]*sf/hub
         sfr = object['subhalos']['SubhaloSFRinRad'][:lim]
 
-        fig = plt.figure(figsize=(8,6))
-        ax = plt.axes(projection='3d')
+        fig = plt.figure(figsize=(8,8))
+        ax = fig.add_subplot(projection='3d')
         ax.grid(True)
 
         ax.scatter3D(x, y, z, c=sfr, cmap='viridis', s=4)
         ax.scatter3D(X, Y, Z, s=10)
-        fig.colorbar(ax.scatter3D(x, y, z, c=np.log(sfr), cmap='viridis', s=4), ax=ax)
+        ax.ticklabel_format(axis='x', style='sci',scilimits=(0,0))
+        ax.ticklabel_format(axis='y', style='sci',scilimits=(0,0))
+        ax.ticklabel_format(axis='z', style='sci',scilimits=(0,0))
+        ax.set_xlabel(r'x [$\times10^{2}\,Mpc$]')
+        ax.set_ylabel(r'y [$\times10^{2}\,Mpc$]')
+        ax.set_zlabel(r'z [$\times10^{2}\,Mpc$]')
         plt.show()
 
     return object
+
+if __name__ == '__main__':
+    readsnap(r'/Users/daksheshkololgi/Library/CloudStorage/OneDrive-UniversityCollegeLondon/Year 1/Illustris/TNG300-1', 99)
--- getting_started.py
+++ getting_started.py
@@ -1,5 +1,5 @@
-import illustris_python as il
-import numpy as np
+    import illustris_python as il
+    import numpy as np
 import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 import plotly.express as px
