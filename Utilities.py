f58f8a5bdf19759f5dfeaf7f0f7770bcdc184a13
Dakshesh.Kololgi <dakshesh.kololgi.23@ucl.ac.uk>
2023-12-09 17:10:34 +0000

    Update plot style and add MST visualization

--- Utilities.py
+++ Utilities.py
@@ -2,10 +2,11 @@ import illustris_python as il
 import mistree as mist
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.lines import Line2D
 import astropy.units as u
 import astropy.constants as c
 import scienceplots
-plt.style.use('science')
+plt.style.use(['science','dark_background'])
 
 def readsnap(path, snapno, xyzplot=True, lim=5000):
     '''Reads the snapshot data from the given path and snapshot number.'''
@@ -33,27 +34,72 @@ def readsnap(path, snapno, xyzplot=True, lim=5000):
         ax = fig.add_subplot(projection='3d')
         ax.grid(True)
         # ax.scatter(x, y, z)
-        ax.scatter(X.to('Mpc'), Y.to('Mpc'), Z.to('Mpc'), marker='o', color='b',s = 10, alpha=0.5, label=f'{lim} Halos')
+        ax.scatter(X.to('Mpc'), Y.to('Mpc'), Z.to('Mpc'), marker='o', color='b',s = 10, alpha=0.3, label = 'Halos')
 
         for i in range(len(assign)):
-            ax.scatter(x[assign[i]].to('Mpc'), y[assign[i]].to('Mpc'), z[assign[i]].to('Mpc'), marker='x', color='r', label='Subhalos')
+            ax.scatter(x[assign[i]].to('Mpc'), y[assign[i]].to('Mpc'), z[assign[i]].to('Mpc'), marker='.',s=5, color='r', label = 'Subhalos')
             
         # ax.scatter3D(x, y, z)#, c=sfr, cmap='viridis', s=4)
         # ax.scatter3D(X, Y, Z)
-        ax.ticklabel_format(axis='x', style='sci',scilimits=(0,0))
-        ax.ticklabel_format(axis='y', style='sci',scilimits=(0,0))
-        ax.ticklabel_format(axis='z', style='sci',scilimits=(0,0))
+        # ax.ticklabel_format(axis='x', style='sci',scilimits=(0,0))
+        # ax.ticklabel_format(axis='y', style='sci',scilimits=(0,0))
+        # ax.ticklabel_format(axis='z', style='sci',scilimits=(0,0))
         ax.set_xlabel(r'x [Mpc]')
         ax.set_ylabel(r'y [Mpc]')
         ax.set_zlabel(r'z [Mpc]')
-        fig.legend()
+        # fig.legend()
         plt.show()
 
     return object
 
-def MST():
-    return 0
+def MST(object, lim=500, xyzplot=True):
+    sf = object['header']['Time'] #time of snapshot
+    hub = object['header']['HubbleParam'] #hubble parameter of simulation
+    X = u.kpc*object['halos']['GroupPos'][:,0][:lim]*sf/hub #spatial coordinates of halos
+    Y = u.kpc*object['halos']['GroupPos'][:,1][:lim]*sf/hub
+    Z = u.kpc*object['halos']['GroupPos'][:,2][:lim]*sf/hub
+    groupids = np.array(object['subhalos']['SubhaloGrNr'])
+
+    assign = [] #list of len lim and each element is a list of subhalos associated with that halo
+    for i in np.arange(lim):
+        assign.append(np.where(groupids==i)[0])
+
+    subhalono = np.sum([len(assign[i]) for i in range(len(assign))])
+    print(f'There are {subhalono} subhalos in {lim} halos.')
+
+    x = u.kpc*object['subhalos']['SubhaloPos'][:,0]*sf/hub #spatial coordinates of subhalos
+    y = u.kpc*object['subhalos']['SubhaloPos'][:,1]*sf/hub
+    z = u.kpc*object['subhalos']['SubhaloPos'][:,2]*sf/hub
+
+    # Initialise MiSTree MST and plot statistics
+    mst = mist.GetMST(x=X, y=Y, z=Z)
+    mst.construct_mst()
+    d, l, b, s, l_index, b_index = mst.get_stats(include_index=True)
+
+    if xyzplot:
+        # Plot the MST nodes and edges
+        fig = plt.figure(figsize=(8,8))
+        ax = fig.add_subplot(projection='3d')
+        ax.grid(False)
+        halos = ax.scatter(X.to('Mpc'), Y.to('Mpc'), Z.to('Mpc'), marker='o', color='b',s = 15, alpha=0.3, label = f'{lim} Halos')        
+        
+        for i in range(len(l_index[0])):
+            ax.plot([X[l_index[0][i]].to('Mpc').value, X[l_index[1][i]].to('Mpc').value], [Y[l_index[0][i]].to('Mpc').value, Y[l_index[1][i]].to('Mpc').value], zs = [Z[l_index[0][i]].to('Mpc').value, Z[l_index[1][i]].to('Mpc').value], color='orange', alpha=0.5)
+        
+        for i in range(len(assign)):
+            ax.scatter(x[assign[i]].to('Mpc'), y[assign[i]].to('Mpc'), z[assign[i]].to('Mpc'), marker='.',s=1, color='r')
+        
+        subhalopatch = Line2D([0], [0], marker='.', color='k', label='Scatter',markerfacecolor='r', markersize=5)
+        MSTpatch = Line2D([0], [0], marker='o', color='orange', label='Scatter',markerfacecolor='k', markersize=0.1)
+
+        ax.set_xlabel(r'x [Mpc]')
+        ax.set_ylabel(r'y [Mpc]')
+        ax.set_zlabel(r'z [Mpc]')
+        ax.legend([halos, subhalopatch, MSTpatch], [f'{lim} Halos',f'{subhalono} Subhalos', 'Halo MST'], loc='upper left')
+        plt.show()
+    return mst
 
 
 if __name__ == '__main__':
-    test=readsnap(r'/Users/daksheshkololgi/Library/CloudStorage/OneDrive-UniversityCollegeLondon/Year 1/Illustris/TNG300-1', 99)
+    test=readsnap(r'/Users/daksheshkololgi/Library/CloudStorage/OneDrive-UniversityCollegeLondon/Year 1/Illustris/TNG300-1', 99, xyzplot=False)
+    MST(test, xyzplot=True)
