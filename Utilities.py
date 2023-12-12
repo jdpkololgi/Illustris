00049f282284f37f436fc6c74295def2e1b14dec
Dakshesh.Kololgi <dakshesh.kololgi.23@ucl.ac.uk>
2023-12-12 17:45:27 +0000

    Add subhalo_MST function to Utilities.py

--- Utilities.py
+++ Utilities.py
@@ -52,7 +52,8 @@ def readsnap(path, snapno, xyzplot=True, lim=5000):
 
     return object
 
-def MST(object, lim=500, xyzplot=True):
+def halo_MST(object, lim=500, xyzplot=True):
+    '''Plots the MST of the halos in the given object.'''
     sf = object['header']['Time'] #time of snapshot
     hub = object['header']['HubbleParam'] #hubble parameter of simulation
     X = u.kpc*object['halos']['GroupPos'][:,0][:lim]*sf/hub #spatial coordinates of halos
@@ -118,7 +119,62 @@ def MST(object, lim=500, xyzplot=True):
         plt.show()
     return mst
 
+def subhalo_MST(object, lim=500000, xyzplot=True):
+    '''Plots the MST of the subhalos in the given object.'''
+    sf = object['header']['Time'] #time of snapshot
+    hub = object['header']['HubbleParam'] #hubble parameter of simulation
+    x = u.kpc*object['subhalos']['SubhaloPos'][:,0][:lim]*sf/hub #spatial coordinates of subhalos
+    y = u.kpc*object['subhalos']['SubhaloPos'][:,1][:lim]*sf/hub
+    z = u.kpc*object['subhalos']['SubhaloPos'][:,2][:lim]*sf/hub
+    uni = 'Mpc'
+
+    # test plotting of subhalos
+    # fig = plt.figure(figsize=(8,8))
+    # ax = fig.add_subplot(projection='3d')
+    # ax.grid(False)
+    # halos = ax.scatter(x.to('Mpc'), y.to('Mpc'), z.to('Mpc'), marker='o', color='red',s = 15, alpha=0.3, label = f'{lim} Subhalos')
+    # ax.set_xlabel(r'x [Mpc]')
+    # ax.set_ylabel(r'y [Mpc]')
+    # ax.set_zlabel(r'z [Mpc]')
+    # plt.show()
+
+    # Initialise MiSTree MST and plot statistics
+    mst = mist.GetMST(x=x.to(uni).value, y=y.to(uni).value, z=z.to(uni).value)
+    mst.construct_mst()
+    d, l, b, s, l_index, b_index = mst.get_stats(include_index=True)
+    
+    # begins by binning the data and storing this in a dictionary.
+    hmst = mist.HistMST()
+    hmst.setup(num_l_bins=25, num_b_bins=20, num_s_bins=15)
+    mst_dict = hmst.get_hist(d, l, b, s)
+
+    # plotting which takes as input the dictionary created before.
+    pmst = mist.PlotHistMST()
+    pmst.read_mst(mst_dict)
+    pmst.plot(usebox=True)
+
+    if xyzplot:
+        # Plot the MST nodes and edges
+        fig = plt.figure(figsize=(8,8))
+        ax = fig.add_subplot(projection='3d')
+        ax.grid(False)
+        halos = ax.scatter(x.to('Mpc'), y.to('Mpc'), z.to('Mpc'), marker='.', color='red',s = 1, alpha=0.1, label = f'{lim} Subhalos')
+        
+        for i in range(len(l_index[0])):
+            ax.plot([x[l_index[0][i]].to('Mpc').value, x[l_index[1][i]].to('Mpc').value], [y[l_index[0][i]].to('Mpc').value, y[l_index[1][i]].to('Mpc').value], zs = [z[l_index[0][i]].to('Mpc').value, z[l_index[1][i]].to('Mpc').value], color='orange', alpha=0.5)
+        
+        subhalopatch = Line2D([0], [0], marker='.', color='k', label='Scatter',markerfacecolor='r', markersize=5)
+        MSTpatch = Line2D([0], [0], marker='o', color='orange', label='Scatter',markerfacecolor='k', markersize=0.1)
+
+        ax.set_xlabel(r'x [Mpc]')
+        ax.set_ylabel(r'y [Mpc]')
+        ax.set_zlabel(r'z [Mpc]')
+        ax.legend([subhalopatch, MSTpatch], [f'{lim} Subhalos', 'Subalo MST'], loc='upper left')
+        plt.show()
+
+
 
 if __name__ == '__main__':
     test=readsnap(r'/Users/daksheshkololgi/Library/CloudStorage/OneDrive-UniversityCollegeLondon/Year 1/Illustris/TNG300-1', 99, xyzplot=False)
-    MST(test, xyzplot=True)
+    # halo_MST(test, xyzplot=True)
+    subhalo_MST(test, xyzplot=True)
