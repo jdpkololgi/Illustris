32c702f0a0bd15704230eccde7af452696f65128
jdpkololgi <kololgi.dakshesh@gmail.com>
2024-02-19 10:14:06 -0800

    Refactor readsnap function to remove unused code and improve readability

--- Utilities.py
+++ Utilities.py
@@ -236,50 +236,6 @@ class cat():
             subhalopatchy = Line2D([0], [0], marker='.', color='k', label='Scatter',markerfacecolor='yellow', markersize=10)
             ax.legend([subhalopatchr, subhalopatchg, subhalopatchb, subhalopatchy], [f'{reds} Void', f'{greens} Wall', f'{blues} Filamentary', f'{yellows} Cluster'], loc='upper left')
             plt.show()            
-         
-def readsnap(path, snapno, xyzplot=True, lim=5000):
-    '''Reads the snapshot data from the given path and snapshot number.'''
-    object = il.groupcat.load(path,snapno)
-
-    if xyzplot:
-        '''Plots in 3D the spatial coordinates of 5000 halos, finds all associated subhalos and plots them in 3D.'''
-        sf = object['header']['Time'] #time of snapshot
-        hub = object['header']['HubbleParam'] #hubble parameter of simulation
-
-        X = u.kpc*object['halos']['GroupPos'][:,0][:lim]*sf/hub #spatial coordinates of halos
-        Y = u.kpc*object['halos']['GroupPos'][:,1][:lim]*sf/hub
-        Z = u.kpc*object['halos']['GroupPos'][:,2][:lim]*sf/hub
-        groupids = np.array(object['subhalos']['SubhaloGrNr'])
-
-        assign = []
-        for i in np.arange(lim):
-            assign.append(np.where(groupids==i))
-
-        x = u.kpc*object['subhalos']['SubhaloPos'][:,0]*sf/hub #spatial coordinates of subhalos
-        y = u.kpc*object['subhalos']['SubhaloPos'][:,1]*sf/hub
-        z = u.kpc*object['subhalos']['SubhaloPos'][:,2]*sf/hub
-
-        fig = plt.figure(figsize=(8,8))
-        ax = fig.add_subplot(projection='3d')
-        ax.grid(True)
-        # ax.scatter(x, y, z)
-        ax.scatter(X.to('Mpc'), Y.to('Mpc'), Z.to('Mpc'), marker='o', color='b',s = 10, alpha=0.3, label = 'Halos')
-
-        for i in range(len(assign)):
-            ax.scatter(x[assign[i]].to('Mpc'), y[assign[i]].to('Mpc'), z[assign[i]].to('Mpc'), marker='.',s=5, color='r', label = 'Subhalos')
-            
-        # ax.scatter3D(x, y, z)#, c=sfr, cmap='viridis', s=4)
-        # ax.scatter3D(X, Y, Z)
-        # ax.ticklabel_format(axis='x', style='sci',scilimits=(0,0))
-        # ax.ticklabel_format(axis='y', style='sci',scilimits=(0,0))
-        # ax.ticklabel_format(axis='z', style='sci',scilimits=(0,0))
-        ax.set_xlabel(r'x [Mpc]')
-        ax.set_ylabel(r'y [Mpc]')
-        ax.set_zlabel(r'z [Mpc]')
-        # fig.legend()
-        plt.show()
-
-    return object
 
 if __name__ == '__main__':
     # test=readsnap(r'/global/homes/d/dkololgi/TNG300-1', 99, xyzplot=False)
