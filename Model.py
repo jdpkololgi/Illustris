e25ada67f7f01774cfdb651830df9edfba086897
Dakshesh.Kololgi <dakshesh.kololgi.23@ucl.ac.uk>
2024-07-19 18:29:12 +0100

    Refactor Model class to include model selection and initialization

--- Model.py
+++ Model.py
@@ -9,3 +9,30 @@ from Network_stats import network
 class Model():
     def __init__(self, model_type = 'mlp'):
         self._net = network()
+        
+        self.model_selector(model_type)
+
+    def __getattr__(self, name):
+        '''
+        Implmenting the __getattr__ method to access the attributes of the Utilities class
+        '''
+        return getattr(self._net, name)
+    
+    def model_selector(self, model_type):
+        '''
+        Function to select the model type
+        '''
+        if model_type == 'mlp':
+            self.model = self.mlp()
+        elif model_type == 'dnn':
+            self.model = 'work in progress'
+        else:
+            raise ValueError('Model type not recognised')
+        
+    def mlp(self):
+        '''
+        Function to create the multi-layer perceptron model
+        '''
+
+        
+        return 0
