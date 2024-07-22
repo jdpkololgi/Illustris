049aecf9540a74b05b9b44b1982e41675c728371
Dakshesh.Kololgi <dakshesh.kololgi.23@ucl.ac.uk>
2024-07-22 18:38:36 +0100

    Refactor Model class to include forward method for network propagation

--- Model_classes.py
+++ Model_classes.py
@@ -18,5 +18,9 @@ class MLP(nn.Module):
             ('fc1', nn.Linear(n_features, n_hidden)),
             ('relu1', nn.ReLU()),
             ('fc2', nn.Linear(n_hidden, n_output_classes)),
-            ('softmax', nn.Softmax(dim = 1))
+            ('softmax', nn.Softmax(dim = 1)) # dim = 1 is the dimension along which the softmax is calculated so all 4 output classes sum to 1
         ]))
+
+    def forward(self, x):
+        # Forward propagate input through the layers
+        return self.layer_stack(x)
