5f7fb5a9d4c20aabb19fa2c534512025848049de
Dakshesh.Kololgi <dakshesh.kololgi.23@ucl.ac.uk>
2024-07-25 03:18:12 +0100

    Adding run method in Model.py

--- Model.py
+++ Model.py
@@ -8,15 +8,11 @@ import torch
 from torch import nn
 
 from Network_stats import network
-from Model_classes import MLP
-
+import Model_classes
 class Model():
     def __init__(self, model_type = 'mlp'):
         self._net = network()
-        
         self.model_selector(model_type)
-        self.device_check()
-
 
     def __getattr__(self, name):
         '''
@@ -29,15 +25,26 @@ class Model():
         Function to select the model type
         '''
         if model_type == 'mlp':
-            self.model = self.mlp()
+            self.model = Model_classes.MLP()
         elif model_type == 'dnn':
             self.model = 'work in progress'
         else:
             raise ValueError('Model type not recognised')
         
-    def mlp(self):
+    def run(self, epochs, learning_rate, mode = 'train'):
         '''
-        Function to create the multi-layer perceptron model
+        Generic function to run different models
         '''
-                
-        return 0
+        # Set the loss and optimiser
+        criterion = nn.CrossEntropyLoss()
+        optimiser = torch.optim.Adam(self.model.parameters(), lr = learning_rate)
+
+        # Load the data
+        self.pipeline()
+
+        if mode == 'train':    
+            # Begin training
+            self.model.train(criterion=criterion, optimiser=optimiser, train_loader=self.train_loader, val_loader=self.val_loader, epochs=epochs)
+        elif mode == 'test':
+            # Begin testing
+            self.model.test(test_loader=self.test_loader)
