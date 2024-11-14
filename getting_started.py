b6b28eb4ffe6bd758e7f94acd1ff6537a09b9faa
Dakshesh.Kololgi <dakshesh.kololgi.23@ucl.ac.uk>
2024-11-14 18:34:48 +0000

    Refactor getting_started.py to improve code organization and enhance 3D plotting functionality

--- getting_started.py
+++ getting_started.py
@@ -1,8 +1,10 @@
-    import illustris_python as il
-    import numpy as np
+import illustris_python as il
+import numpy as np
 import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 import plotly.express as px
+import astropy.units as u
+import astropy.constants as c
 import scienceplots
 plt.style.use('science')
 
@@ -10,18 +12,17 @@ plt.style.use('science')
 basePath = './Illustris-3/output/'
 fields = ['SubhaloMass','SubhaloSFRinRad']
 subhalos = il.groupcat.loadSubhalos(basePath,135,fields=fields)
-
 print(subhalos.keys())
 print(subhalos['SubhaloMass'].shape)
 
-mass_msun = subhalos['SubhaloMass'] * 1e10 / 0.704
-plt.plot(mass_msun,subhalos['SubhaloSFRinRad'],'.')
-plt.xscale('log')
-plt.yscale('log')
-plt.yscale('log')
-plt.xlabel('Total Mass [$M_\odot$]')
-plt.ylabel('Star Formation Rate [$M_\odot / yr$]')
-plt.show()
+# mass_msun = subhalos['SubhaloMass'] * 1e10 / 0.704
+# plt.plot(mass_msun,subhalos['SubhaloSFRinRad'],'.')
+# plt.xscale('log')
+# plt.yscale('log')
+# plt.yscale('log')
+# plt.xlabel('Total Mass [$M_\odot$]')
+# plt.ylabel('Star Formation Rate [$M_\odot / yr$]')
+# plt.show()
 
 GroupFirstSub = il.groupcat.loadHalos(basePath,135,fields=['GroupFirstSub'])
 ptNumGas = il.snapshot.partTypeNum('gas') # 0
@@ -36,30 +37,43 @@ for i in range(5):
 #Illustris300simulation data
 basePath2 = r'/Users/daksheshkololgi/Library/CloudStorage/OneDrive-UniversityCollegeLondon/Year 1/Illustris/TNG300-1'
 object = il.groupcat.load(basePath2,99)
+print(object.keys())
 
-plt.plot(object['subhalos']['SubhaloMass'], object['subhalos']['SubhaloSFRinRad'],'x')
-plt.xscale('log')
-plt.yscale('log')
-plt.show()
+# plt.plot(object['subhalos']['SubhaloMass'], object['subhalos']['SubhaloSFRinRad'],'x')
+# plt.xscale('log')
+# plt.yscale('log')
+# plt.show()
+
+sf = object['header']['Time'] #time (scale factor) of snapshot
+hub = object['header']['HubbleParam'] #hubble parameter of simulation
 
 #3D plot
-lim = 5000
-x = object['subhalos']['SubhaloPos'][:,0][:lim]
-y = object['subhalos']['SubhaloPos'][:,1][:lim]
-z = object['subhalos']['SubhaloPos'][:,2][:lim]
-sfr = object['subhalos']['SubhaloSFRinRad'][:lim]
+# lim = 5000
 
-df = {'x':x, 'y':y, 'z':z, 'sfr':sfr}
+# sfr = object['subhalos']['SubhaloSFRinRad'][:lim]
+masscut = 1e9 #mass cut for subhalos
+stars = (object['subhalos']['SubhaloMassType'][:,4]) #stellar mass of subhalos
+mc = masscut*hub/1e10 #mass cut for subhalos
+stars_indices = np.where(stars>=mc)[0] #indices of subhalos with stellar mass greater than masscut
+x = (u.kpc*object['subhalos']['SubhaloPos'][:,0]*sf/hub)[stars_indices]
+y = (u.kpc*object['subhalos']['SubhaloPos'][:,1]*sf/hub)[stars_indices]
+z = (u.kpc*object['subhalos']['SubhaloPos'][:,2]*sf/hub)[stars_indices]
+
+df = {'x':x, 'y':y, 'z':z}
 
 # fig = go.Figure(data=[go.Scatter3d(x=x, y=y, z=z, mode='markers', marker=dict(size=4, color=sfr, opacity=0.8))])
 # fig.show()
 
-fig = plt.figure(figsize=(8,6))
-ax = plt.axes(projection='3d')
-ax.grid(True)
+fig = plt.figure(figsize=(10,10))
+ax = fig.add_subplot(projection='3d')
+ax.scatter(x.to('Mpc'), y.to('Mpc'), z.to('Mpc'), marker='.',s=5, color='r', label = 'Subhalos')
 
-ax.scatter3D(x, y, z, c=sfr, cmap='viridis', s=4)
-fig.colorbar(ax.scatter3D(x, y, z, c=np.log(sfr), cmap='viridis', s=4), ax=ax)
+
+
+
+# ax.scatter3D(x, y, z, c=sfr, cmap='viridis', s=4)
+# ax.scatter3D(x, y, z, s=4)
+# fig.colorbar(ax.scatter3D(x, y, z, c=np.log(sfr), cmap='viridis', s=4), ax=ax)
 plt.show()
 
 # fig2 = px.scatter_3d(df, x='x', y='y', z='z', color='sfr', opacity=0.8, size_max=2)
